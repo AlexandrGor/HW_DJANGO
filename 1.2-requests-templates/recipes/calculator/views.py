789f0729cc804a870f84654a0dbdf8cb4cbf409b
Alexandr Gorbishev <alexandrgorbishev@inbox.ru>
2022-02-15 23:48:18 +0300

    1.2-request-templates

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -19,12 +19,14 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def dish_view(request, dish):
+    servings = int(request.GET.get("servings", 1))
+    data = {}
+    if dish in DATA:
+        data = DATA[dish]
+        for key in data:
+            data[key] *= servings
+    context = {
+        'recipe': data
+    }
+    return render(request, 'calculator/index.html', context)
